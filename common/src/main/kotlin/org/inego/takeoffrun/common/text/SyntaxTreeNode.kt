9830b3ea2919b68a486f9589356dbc1d8704665c
Yanis Batura <yanis.batura@gmail.com>
2020-01-09 09:08:23 +0700

    Improve rules format

--- common/src/main/kotlin/org/inego/takeoffrun/common/text/SyntaxTreeNode.kt
+++ common/src/main/kotlin/org/inego/takeoffrun/common/text/SyntaxTreeNode.kt
@@ -1,5 +1,9 @@
 package org.inego.takeoffrun.common.text
 
+import org.inego.takeoffrun.common.language.SyntaxTreeNodeType
+
 interface SyntaxTreeNode {
     val children: List<SyntaxTreeNode>
+    val nodeType: SyntaxTreeNodeType
+    val mainChild: SyntaxTreeNode?
 }
--- common/src/main/kotlin/org/inego/takeoffrun/common/text/impl/WordNode.kt
+++ common/src/main/kotlin/org/inego/takeoffrun/common/text/impl/WordNode.kt
@@ -1,12 +1,14 @@
 package org.inego.takeoffrun.common.text.impl
 
 import org.inego.takeoffrun.common.language.GrammaticalFeatureValue
+import org.inego.takeoffrun.common.language.SyntaxTreeNodeType
 import org.inego.takeoffrun.common.language.Word
 import org.inego.takeoffrun.common.text.SyntaxTreeNode
 
-class WordNode(val word: Word) : SyntaxTreeNode {
+class WordNode(val word: Word, override val nodeType: SyntaxTreeNodeType) : SyntaxTreeNode {
     val externalFeatures = arrayListOf<GrammaticalFeatureValue>()
     override val children: List<SyntaxTreeNode>
         get() = emptyList()
-
+    override val mainChild: SyntaxTreeNode?
+        get() = null
 }
